d3c7d4c750c03b3cd5ff8582f7c7e4b388c7e77a
wooplus <aunglinhtet992@gmail.com>
2023-02-24 13:22:08 -0500

    level 4 - bcrypt with hash

--- app.js
+++ app.js
@@ -7,9 +7,10 @@ const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
-const encrypt = require("mongoose-encryption");
 const { env } = require("process");
 
 app.use(express.static("public"));
@@ -41,24 +42,21 @@ const userSchema = new mongoose.Schema({
   password: String,
 });
 
-userSchema.plugin(encrypt, {
-  secret: process.env.SECRET,
-  encryptedFields: ["password"],
-});
-
 const User = new mongoose.model("User", userSchema);
 
 app.post("/register", function (req, res) {
-  let newUser = new User({
-    email: req.body.username,
-    password: req.body.password,
-  });
-  newUser.save(function (err) {
-    if (err) {
-      console.log(err);
-    } else {
-      res.render("secrets");
-    }
+  bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+    let newUser = new User({
+      email: req.body.username,
+      password: hash,
+    });
+    newUser.save(function (err) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.render("secrets");
+      }
+    });
   });
 });
 
@@ -70,10 +68,15 @@ app.post("/login", function (req, res) {
     if (err) {
       console.log(err);
     } else {
-      if (foundUser.password === password) {
-        res.render("secrets");
-      } else {
-        res.send("th password is wrong, please try again.");
+      if (foundUser) {
+        // Load hash from your password DB.
+        bcrypt.compare(password, foundUser.password, function (err, result) {
+          if (result) {
+            res.render("secrets");
+          } else {
+            res.send("Your password is wrong");
+          }
+        });
       }
     }
   });
